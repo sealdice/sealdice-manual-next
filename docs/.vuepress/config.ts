59e17d6da1218e7303acbfaf8dbcce647df00af7
JustAnotherID <just-another-id@outlook.com>
2023-11-06 11:31:08 +0800

    feat: 新增聊天框组件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,9 @@
 import { defineUserConfig } from "vuepress";
+import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
 // @ts-ignore
 import { searchProPlugin } from "vuepress-plugin-search-pro";
+import { path } from "@vuepress/utils";
+
 import theme from "./theme";
 
 const basePath: any = process.env.BASE_PATH ?? "/sealdice-manual-next/";
@@ -14,6 +17,11 @@ export default defineUserConfig({
   theme,
 
   plugins: [
+    registerComponentsPlugin({
+      components: {
+        ChatBox: path.resolve(__dirname, "./components/ChatBox.vue"),
+      },
+    }),
     searchProPlugin({
       indexContent: true,
       autoSuggestions: true,
