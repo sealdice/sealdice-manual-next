f03e0a421fad1819b5b38eaa83c6bb269b52cf49
MX-fox <1009592348@qq.com>
2024-04-14 21:42:23 +0800

    新增安卓端海豹相关内容

--- docs/.vitepress/catalogue.ts
+++ docs/.vitepress/catalogue.ts
@@ -16,6 +16,7 @@ export const deployNav = {
         { text: "迁移", link: "/deploy/transfer" },
         { text: "特色功能", link: "/deploy/special_feature" },
         { text: "数据库检查和修复", link: "/deploy/db-repair" },
+        { text: "安卓端海豹相关", link: "/deploy/android" },
         { text: "配置安卓端保活", link: "/deploy/android_keepalive"},
       ],
     },
@@ -52,6 +53,7 @@ export const deploySidebar = {
         { text: "迁移", link: "/deploy/transfer" },
         { text: "特色功能", link: "/deploy/special_feature" },
         { text: "数据库检查和修复", link: "/deploy/db-repair" },
+        { text: "安卓端海豹相关", link: "/deploy/android" },
         { text: "配置安卓端保活", 
           link: "/deploy/android_keepalive" ,
           items: [
